47962624a0cc31c12b174e0945e150f6c318cf3b
Grzegorz Bielski <pesiok@gmail.com>
2020-03-15 22:12:29 +0100

    set coverage

---
+++ src/main/scala/approximation/SetCoverage.scala
@@ -0,0 +1,34 @@
+package approximation
+
+import scala.annotation.tailrec
+
+object SetCoverage {
+  type Available[K, V] = Map[K, Set[V]]
+  type Values[V] = Set[V]
+
+  def apply[K, V](available: Available[K, V], toCover: Values[V]): Values[K] = {
+
+    @tailrec
+    def go(
+        toCover: Values[V],
+        covered: Values[K]
+    ): Values[K] =
+      if (toCover.size == 0) covered
+      else {
+        val res =
+          available.foldLeft[(Option[K], Set[V])]((None, Set()))(
+            (acc, curr) => {
+              val coveredPrim = toCover `intersect` curr._2
+              if (coveredPrim.size > acc._2.size) (Some(curr._1), coveredPrim)
+              else acc
+            })
+
+        go(
+          toCover `diff` res._2,
+          res._1.map(covered + _) getOrElse covered
+        )
+      }
+
+    go(toCover, Set())
+  }
+}
