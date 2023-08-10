0fbf67c2fa2f50552ff4b51052afa589505b9595
Grzegorz Bielski <pesiok@gmail.com>
2023-08-10 14:28:26 +0200

    knapsack

---
+++ src/main/scala/algos/small/Knapsack.scala
@@ -0,0 +1,41 @@
+package algos.small
+
+object Knapsack:
+  // brute force approach: O(2^n)
+  // where n is the number of items
+
+  // dynamic programming approach: O(n * W)
+  // where n is the number of items and W is the capacity of the knapsack
+
+  // dynamic programming - solve subproblems, store them and build up to the solution to the original problem
+
+  final case class Item(name: String, weight: Int, value: Int)
+
+  def findBestCombination(items: Vector[Item], maxCapacity: Int): Vector[Item] =
+    // table[combinations of items][max capacity] = summed value
+    val table = Array.ofDim[Int](items.size + 1, maxCapacity + 1)
+
+    // build the solutions table
+    for
+      i <- items.indices
+      item = items(i)
+      c <- 1 to maxCapacity
+    yield
+      val curr = table(i)(c)
+      val next = if c >= item.weight then math.max(item.value + table(i)(c - item.weight), curr) else curr
+
+      table(i + 1)(c) = next
+
+    val bestCombination = Vector.newBuilder[Item]
+
+    // start at the bottom right corner of the table
+    (items.size until 0 by -1).foldLeft(maxCapacity): (c, i) =>
+      // check if there was a change in the value inserted into the table
+      // if it was - the item was the best choice in that combination
+
+      if table(i - 1)(c) != table(i)(c) then
+        bestCombination += items(i - 1)
+        c - items(i - 1).weight // move up the table
+      else c
+
+    bestCombination.result()
---
+++ src/test/scala/algos/small/KnapsackSpec.scala
@@ -0,0 +1,35 @@
+package algos.small
+
+import org.scalatest.*, funsuite.*, matchers.*
+
+class KnapsackSpec extends AnyFunSuite, should.Matchers:
+  test("finds the best combination in terms if resources / value within max capacity"):
+    val items = Vector(
+      ("television", 50, 500),
+      ("stereo", 35, 400),
+      ("laptop", 3, 1000),
+      ("food", 15, 50),
+      ("clothing", 20, 800),
+      ("jewelry", 1, 4000),
+      ("books", 100, 300),
+      ("printer", 18, 30),
+      ("refrigerator", 200, 700),
+      ("painting", 10, 1000),
+      ("candlesticks", 2, 300)
+    ).map(Knapsack.Item.apply.tupled)
+
+    val result = Knapsack.findBestCombination(
+      items = items,
+      maxCapacity = 75
+    )
+
+    result shouldBe Vector(
+      Knapsack.Item("candlesticks", 2, 300),
+      Knapsack.Item("painting", 10, 1000),
+      Knapsack.Item("jewelry", 1, 4000),
+      Knapsack.Item("clothing", 20, 800),
+      Knapsack.Item("laptop", 3, 1000),
+      Knapsack.Item("stereo", 35, 400)
+    )
+
+    assert(result.map(_.weight).sum <= 75)
